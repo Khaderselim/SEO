f3a683bef9613a7746b087c865b0e2f5b80aac25
Khader Mohamed Selim <103784364+Khaderselim@users.noreply.github.com>
2025-04-16 10:24:35 +0100

    test param_test.py in the server

--- Web-scraping/Web-driver-solution/param_test.py
+++ Web-scraping/Web-driver-solution/param_test.py
@@ -8,7 +8,7 @@ def test_method():
         page = browser.new_page()
         try:
             page.goto("https://www.mytek.tn/trottinette-electrique-kepow-e9pro10s-noir.html", timeout=30000)
-            page.wait_for_timeout(2000)
+            page.wait_for_timeout(5000)  # Wait for 5 seconds to allow the page to load
             html_content = page.content()
         except Exception as e:
             print(f"Error loading page: {e}")
